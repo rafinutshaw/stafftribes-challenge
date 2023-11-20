36be1f728ba62666a66bb2e5a30f970367059c13
Rafin Akther Utsahw <rafin.utshaw@gmail.com>
2023-11-21 00:07:11 +0200

    Scaffold app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div>My friends
+      </div>
+      <div>MY AVAILABILITY FOR THE NEXT 7 WEEKS
+      </div>
+    </main>
+  )
+}
